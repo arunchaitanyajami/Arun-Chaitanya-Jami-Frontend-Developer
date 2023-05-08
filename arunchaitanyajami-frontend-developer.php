444f39122696ab2d2ae08ccfdd35db180ce598b3
Arun Chaitanya Jami <arunchaitanyajami@gmail.com>
2023-05-08 15:18:58 +0530

    Setup composer with Phpunit and PHPCS using VIP GO STANDARDS

--- arunchaitanyajami-frontend-developer.php
+++ arunchaitanyajami-frontend-developer.php
@@ -11,3 +11,7 @@
  *
  * @package         WordPress
  */
+
+namespace Arunchaitanyajami\FrontEndDeveloper;
+
+require 'vendor/autoload.php';
---
+++ tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+// First we need to load the composer autoloader, so we can use WP Mock
+require_once dirname(__DIR__).'/vendor/autoload.php';
+
+const HOUR_IN_SECONDS = 3600;
+
+// Bootstrap WP_Mock to initialize built-in features
+WP_Mock::setUsePatchwork( true );
+WP_Mock::bootstrap();
+
+// Optional step
+// If your project does not use autoloading via Composer, include your files now
+require_once dirname( dirname( __FILE__ ) ) . '/arunchaitanyajami-frontend-developer.php';
