7655e27622dcbe5b736a0527dffe19e720b3d054
Arun Chaitanya Jami <arunchaitanyajami@gmail.com>
2023-05-08 15:58:01 +0530

    Create Rest Endpoint and hide it from public

--- arunchaitanyajami-frontend-developer.php
+++ arunchaitanyajami-frontend-developer.php
@@ -15,3 +15,40 @@
 namespace Arunchaitanyajami\FrontEndDeveloper;
 
 require 'vendor/autoload.php';
+
+use Arunchaitanyajami\FrontEndDeveloper\RestApi\SpaceXDataProvider;
+use Arunchaitanyajami\FrontEndDeveloper\RestApi\DataFetcher;
+
+
+/**
+ * Register Custom REST API endpoint to fetch data from SpaceX Endpoint.
+ */
+add_action(
+	'rest_api_init',
+	function () {
+		register_rest_route(
+			'custom/v1',
+			'/data/',
+			array(
+				'methods'             => 'GET',
+				'callback'            => function ( \WP_REST_Request $request ) {
+					$filter_by = $request->get_param( 'filter_by' );
+					$filter_by = ! empty( $filter_by ) ? $filter_by : '';
+
+					$filter_value = $request->get_param( 'filter_value' );
+					$filter_value = ! empty( $filter_value ) ? $filter_value : '';
+
+					$page = $request->get_param( 'page' );
+					$page = ! empty( $page ) ? $page : 1;
+
+					$data_fetcher = new DataFetcher( new SpaceXDataProvider() );
+					// Fetch data using data fetcher service.
+					return $data_fetcher->get_capsules_data( $page, $filter_by, $filter_value );
+				},
+				'permission_callback' => function () {
+					return ! is_admin() || is_user_logged_in();
+				},
+			)
+		);
+	}
+);
---
+++ inc/RestApi/DataFetcher.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * DataFetcher Class.
+ *
+ * This class is responsible for fetching data from cached data or else if the data is empty then fetch data from
+ * DataProviderInterface.
+ *
+ * @package WordPress
+ */
+
+namespace Arunchaitanyajami\FrontEndDeveloper\RestApi;
+
+/**
+ * DataFetcher Class.
+ */
+class DataFetcher {
+
+
+	/**
+	 * Pagination limit.
+	 *
+	 * @var int
+	 */
+	private int $max_length = 10;
+
+	/**
+	 * Data provider interface.
+	 *
+	 * @var DataProviderInterface
+	 */
+	private DataProviderInterface $data_provider;
+
+	/**
+	 * Class Constructor.
+	 *
+	 * @param DataProviderInterface $data_provider Data provider.
+	 */
+	public function __construct( DataProviderInterface $data_provider ) {
+		$this->data_provider = $data_provider;
+	}
+
+	/**
+	 * Retrieves the cached data or data from spacex.
+	 *
+	 * @param int    $page Page Number.
+	 * @param string $filter_by Key to filter the array.
+	 * @param string $filter_value Value to Filter data based on the key selected.
+	 *
+	 * @return array
+	 */
+	public function get_capsules_data( int $page = 1, string $filter_by = '', string $filter_value = '' ): array {
+		// Get cache key.
+		$cache_key = 'spacex_all_capsules_api_data';
+
+		// Try to get data from cache.
+		$data = get_transient( $cache_key );
+
+		// If cache is empty, fetch data and set cache.
+		if ( false === $data ) {
+			// Get data from data provider.
+			$data = $this->data_provider->get_all_capsules_data();
+
+			// Set cache for 1 hour.
+			set_transient( $cache_key, $data['data'], 1 * \HOUR_IN_SECONDS );
+		}
+
+		if ( empty( $data ) ) {
+			return array();
+		}
+
+		if ( $filter_by && $filter_value ) {
+			$data = array_filter(
+				$data,
+				function ( $item ) use ( $filter_by, $filter_value ) {
+					return $item[ $filter_by ] == $filter_value;
+				}
+			);
+		}
+
+		if ( empty( $data ) ) {
+			return array();
+		}
+
+		$offset = ( $page - 1 ) * $this->max_length;
+		$data   = array_values( $data );
+
+		// Return data.
+		return count( $data ) > $this->max_length ? array_slice( $data, $offset, $this->max_length ) : $data;
+	}
+}
