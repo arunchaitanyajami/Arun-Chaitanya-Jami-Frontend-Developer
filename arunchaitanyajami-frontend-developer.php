5ed35a0e8e6769c7cd39a3022878e07a06966e97
Arun Chaitanya Jami <arunchaitanyajami@gmail.com>
2023-05-08 14:48:42 +0530

    SetUp Plugin Base file

---
+++ arunchaitanyajami-frontend-developer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     arunchaitanyajami-frontend-developer
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     WordPress Gutenberg Block to share SpaceX data with logged-in users.
+ * Author:          arunchaitanyajami
+ * Author URI:      https://github.com/arunchaitanyajami
+ * Text Domain:     arunchaitanyajami-frontend-developer
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         WordPress
+ */
